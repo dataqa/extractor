0f92f30da1e4d63b11118bf9a71a43d3be1d2266
mrm1001 <mariarmestre@gmail.com>
2023-12-06 16:00:50 +0000

    version bump

--- dataqa_extractor/extract.py
+++ dataqa_extractor/extract.py
@@ -36,7 +36,7 @@ def define_extractor_class(fields, extraction_summary):
     return ExtractedDataClass
 
 
-def process_file(openai_api_key, uploaded_file, output_file_path, fields, extraction_summary):
+def process_file(openai_api_key, uploaded_file, fields, extraction_summary):
     ExtractedDataClass = define_extractor_class(fields, extraction_summary)
     client = get_open_ai_client(openai_api_key)
     count_extractions = Counter()
@@ -46,59 +46,60 @@ def process_file(openai_api_key, uploaded_file, output_file_path, fields, extrac
 
         total_cost = 0
 
-        with open(output_file_path, "w") as output:
-            writer = csv.writer(output)
-            example = {}
-            column_names = []
+        output = io.StringIO()
+        writer = csv.writer(output)
+        example = {}
+        column_names = []
 
-            for line_ind, line in enumerate(csv_reader):
-                if len(line) > 0:
-                    text = line[0]
+        for line_ind, line in enumerate(csv_reader):
+            if len(line) > 0:
+                text = line[0]
 
-                    completion = client.ChatCompletion.create(
-                        model=MODEL,
-                        functions=[ExtractedDataClass.openai_schema],
-                        function_call={"name": ExtractedDataClass.openai_schema["name"]},
-                        messages=[
-                            {
-                                "role": "system",
-                                "content": f"I'm going to ask for {ExtractedDataClass.__doc__}. Use {ExtractedDataClass.openai_schema['name']} to parse this data.",
-                            },
-                            {
-                                "role": "user",
-                                "content": text,
-                            },
-                        ],
-                    )
+                completion = client.ChatCompletion.create(
+                    model=MODEL,
+                    functions=[ExtractedDataClass.openai_schema],
+                    function_call={"name": ExtractedDataClass.openai_schema["name"]},
+                    messages=[
+                        {
+                            "role": "system",
+                            "content": f"I'm going to ask for {ExtractedDataClass.__doc__}. Use {ExtractedDataClass.openai_schema['name']} to parse this data.",
+                        },
+                        {
+                            "role": "user",
+                            "content": text,
+                        },
+                    ],
+                )
 
-                    prompt_tokens = completion.usage.prompt_tokens
-                    completion_tokens = completion.usage.completion_tokens
+                prompt_tokens = completion.usage.prompt_tokens
+                completion_tokens = completion.usage.completion_tokens
 
-                    if MODEL in MODEL_COSTS:
-                        total_cost += (prompt_tokens * MODEL_COSTS[MODEL]["input"] + completion_tokens *
-                                       MODEL_COSTS[MODEL]["output"]) / 1000
+                if MODEL in MODEL_COSTS:
+                    total_cost += (prompt_tokens * MODEL_COSTS[MODEL]["input"] + completion_tokens *
+                                   MODEL_COSTS[MODEL]["output"]) / 1000
 
-                    user_details = ExtractedDataClass.from_response(completion)
-                    line_values = user_details.model_dump()
+                user_details = ExtractedDataClass.from_response(completion)
+                line_values = user_details.model_dump()
 
-                    if line_ind == 0:
-                        column_names = list(line_values.keys())
-                        writer.writerow(column_names)
-                        example = dict((k, line_values[k]) for k in column_names)
-                        example["original_text"] = text
+                if line_ind == 0:
+                    column_names = list(line_values.keys())
+                    writer.writerow(column_names)
+                    example = dict((k, line_values[k]) for k in column_names)
+                    example["original_text"] = text
 
-                    output_line = []
-                    for col in column_names:
-                        value = line_values[col]
-                        if value:
-                            count_extractions[col] += 1
-                        output_line.append(value)
-                    writer.writerow(output_line)
+                output_line = []
+                for col in column_names:
+                    value = line_values[col]
+                    if value:
+                        count_extractions[col] += 1
+                    output_line.append(value)
+                writer.writerow(output_line)
 
     return {"total_lines": line_ind + 1,
             "total_cost": total_cost,
             "example": example,
-            "total_extractions": count_extractions}
+            "total_extractions": count_extractions,
+            "csv": output}
 
 
 def check_fields(fields):
@@ -118,10 +119,6 @@ def streamlit_app():
 
         openai_api_key = st.text_input("Enter a valid OpenAI API key:", type="password")
         uploaded_file = st.file_uploader("Choose a csv file", accept_multiple_files=False, type="csv")
-
-        output_folder_path = st.text_input(
-            "Enter the folder path where you want to save the results:", HOME + "/Downloads"
-        )
 
         num_cols = st.number_input(
             "Number fields to extract", min_value=1, max_value=20, value=None
@@ -161,29 +158,31 @@ def streamlit_app():
                     if not check_fields(fields):
                         st.warning("The name, type and description of a field need to be filled.")
                     else:
-                        output_file_path = f"{output_folder_path}/extracted_data.csv"
                         result = process_file(openai_api_key,
                                               uploaded_file,
-                                              output_file_path,
                                               fields,
                                               extraction_summary)
-                        with st.container(border=True):
-                            st.subheader("Results")
-                            if result["total_cost"] > 0:
-                                st.write(
-                                    f"Extracting values from {result['total_lines']} lines cost an estimated ${result['total_cost']:0.2f}.")
-                            else:
-                                st.write(
-                                    f"Extracting values from {result['total_lines']} lines.")
-                            st.write(f"Results written to {output_file_path}")
-                            st.markdown("**Summary extractions**")
-                            st.table(pd.DataFrame.from_records([result["total_extractions"]]))
-                            if result["example"]:
-                                st.markdown("**Example**")
-                                st.table(pd.DataFrame.from_records([result["example"]]))
 
                 else:
                     st.warning("Need to upload csv file")
+
+        if submitted:
+
+            with st.container(border=True):
+                st.subheader("Results")
+                st.download_button('Download CSV', result["csv"].getvalue(), file_name="extracted_data.csv",
+                                   mime='text/csv')
+                if result["total_cost"] > 0:
+                    st.write(
+                        f"Extracting values from {result['total_lines']} lines cost an estimated ${result['total_cost']:0.2f}.")
+                else:
+                    st.write(
+                        f"Extracting values from {result['total_lines']} lines.")
+                st.markdown("**Summary extractions**")
+                st.table(pd.DataFrame.from_records([result["total_extractions"]]))
+                if result["example"]:
+                    st.markdown("**Example**")
+                    st.table(pd.DataFrame.from_records([result["example"]]))
 
 
 if __name__ == "__main__":
